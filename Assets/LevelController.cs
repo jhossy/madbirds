331567f0191d0df3e6fcf9565ff0363668cac49a
jhossy <hossy@godmail.dk>
2021-12-15 19:21:31 +0100

    added level2

---
+++ Assets/LevelController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies;    
+
+    private void OnEnable()
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Enemy enemy in _enemies)
+        {
+            if(enemy != null) //enemy objects becomes NULL when Destroyed
+            {
+                return;
+            }
+        }
+
+        Debug.Log("You killed all enemies");
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
